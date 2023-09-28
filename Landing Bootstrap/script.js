ce552f824ba5033e82c21e053050a1f8b5ac382c
AndreaAlvigini <andreaalvigini@gmail.com>
2023-09-28 17:55:21 +0200

    versione con jquery

--- Landing Bootstrap/script.js
+++ Landing Bootstrap/script.js
@@ -1,35 +1,25 @@
-// Aggiunge un gestore di eventi che si attiva quando il documento HTML è completamente caricato
-window.addEventListener("DOMContentLoaded", (event) => {
+$(document).ready(function() {
     //SLIDE IN TEXT 
     function textSlide(id, delay) {
-        const subtitle = document.getElementById(id);
+        const subtitle = $("#" + id);
         setTimeout(function () {
-            subtitle.style.opacity = "1";
-            subtitle.style.transform = "translateY(0)";
+            subtitle.css("opacity", "1");
+            subtitle.css("transform", "translateY(0)");
         }, delay);
     }
     textSlide("subtitle", 200);
     textSlide("maintitle", 400);
 
-
     //HAMBURGER AUTOCLOSE
     function viewportWidthCustom(customWidth) {
-        let viewport = window.innerWidth;
+        let viewport = $(window).width();
         if (viewport < customWidth) {
-            // console.log(viewport + "TEST");
-            const navbarToggler = document.getElementById("navbar-toggler");
-            // Seleziona tutti i link del menu
-            const menuLinks = document.querySelectorAll(".navbar-nav .nav-link");
-            // Aggiungi un gestore di eventi clic a ciascun link del menu
-            menuLinks.forEach((link) => {
-                link.addEventListener("click", () => {
-                    // Chiudi il menu hamburger dopo il clic
-                    if (navbarToggler.classList.contains("collapsed")) {
-                        return; // Il menu è già chiuso, esci
-                    }
-                    navbarToggler.click(); // Simula il clic sul pulsante dell'hamburger menu
-                    click.preventDefault(); // Impedisce il comportamento predefinito del link
-                });
+            const navbarToggler = $("#navbar-toggler");
+            const menuLinks = $(".navbar-nav .nav-link");
+            menuLinks.on("click", function() {
+                if (!navbarToggler.hasClass("collapsed")) {
+                    navbarToggler.trigger("click");
+                }
             });
         }
     }
@@ -37,64 +27,47 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     //NAVBAR Trasparent move
     function navBarTransition() {
-        const navbar = document.querySelector(".navbar");
+        const navbar = $(".navbar");
 
-        window.addEventListener("scroll", () => {
-            let viewport = window.innerWidth;
-            console.log(viewport);
-                const scrollY = window.scrollY;
-                const maxScroll = 150; // Puoi regolare questo valore per determinare quando raggiungere il colore bianco completamente
-                // Calcola il colore di sfondo in base allo scroll
-                const backgroundColor = `rgba(235, 235, 235, ${Math.min(scrollY / maxScroll, 1)})`;
-                // Applica il colore di sfondo alla navbar
-                navbar.style.backgroundColor = backgroundColor;
-                // Cambia la classe della navbar in base al colore di sfondo
-                if (scrollY >= maxScroll) {
-                    navbar.classList.remove("transparent");
-                    navbar.classList.add("white");
-                } else {
-                    navbar.classList.add("transparent");
-                    navbar.classList.remove("white");
-                }
+        $(window).scroll(function () {
+            let viewport = $(window).width();
+            const scrollY = $(window).scrollTop();
+            const maxScroll = 150;
+            const backgroundColor = `rgba(235, 235, 235, ${Math.min(scrollY / maxScroll, 1)})`;
+            navbar.css("background-color", backgroundColor);
+
+            if (scrollY >= maxScroll) {
+                navbar.removeClass("transparent").addClass("white");
+            } else {
+                navbar.addClass("transparent").removeClass("white");
+            }
         });
     }
     navBarTransition();
 
     //PROGRESSBAR
-    // Seleziona tutti gli elementi HTML con la classe CSS "progress"
-    document.querySelectorAll(".progress").forEach((item) => {
-        console.log("Test");
-        // Ottieni il valore dell'attributo "data-value" dell'elemento corrente
-        let value = item.getAttribute("data-value");
-        // Trova l'elemento figlio con classe "progress-bar" all'interno dello stesso elemento
-        let bar = item.querySelector(".progress-bar");
-        // Imposta la larghezza della barra di avanzamento con il valore ottenuto
-        bar.style.width = value;
-        // Aggiungi un ritardo di 500 millisecondi prima di reimpostare la larghezza
+    $(".progress").each(function () {
+        let value = $(this).attr("data-value");
+        let bar = $(this).find(".progress-bar");
+        bar.css("width", value);
         setTimeout(function () {
-            bar.style.width = value;
+            bar.css("width", value);
         }, 500);
     });
 
+    //SCROLL TO TOP
+    let mybutton = $("#myBtn");
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 20) {
+            mybutton.css("display", "block");
+        } else {
+            mybutton.css("display", "none");
+        }
+    });
+
+    mybutton.click(function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 500);
+    });
 });
-
-//SCROLL TO TOP
-// Get the button
-let mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
